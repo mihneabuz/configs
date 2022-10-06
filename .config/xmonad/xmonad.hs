fe69e3f703ef053596fd725e9157479ed3ed6589
mihneabuz <mihneabuzatu88@gmail.com>
2022-10-06 23:28:38 +0300

    fixed xmonad compilation

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,4 +1,28 @@
 import XMonad
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
 
 main :: IO ()
-main = xmonad def
+main = xmonad . xmobarProp $ myConfig
+
+myConfig =
+  def
+    { modMask = mod4Mask,
+      terminal = "kitty",
+      layoutHook = myLayout
+    }
+    `additionalKeysP` [ ("M-b", spawn "brave")
+                      ]
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled = Tall nmaster delta ratio
+    nmaster = 1 -- Default number of windows in the master pane
+    ratio = 1 / 2 -- Default proportion of screen occupied by master pane
+    delta = 3 / 100 -- Percent of screen to increment by when resizing panes
