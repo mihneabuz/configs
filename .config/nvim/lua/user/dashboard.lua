3925fbf44b561d808bc61f7dd6ea2237b7c1226c
mihneabuz <mihneabuzatu88@gmail.com>
2023-02-25 22:18:52 +0200

    working on volume in waybar

--- .config/nvim/lua/user/dashboard.lua
+++ .config/nvim/lua/user/dashboard.lua
@@ -24,16 +24,16 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-    dashboard.button("e", "    New file" , ":ene<CR>"),
-    dashboard.button("f", "    Find file", ":Telescope find_files<CR>"),
-    dashboard.button("t", "פּ    File Tree", ":ene<CR>:NvimTreeOpen<CR>"),
-    dashboard.button("r", "    Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button("p", "    Projects" , ":Telescope projects<CR>"),
-    dashboard.button("s", "    Settings" , ":e $MYVIMRC<CR>"),
-    dashboard.button("k", "    Keybinds" , ":Keybinds<CR>"),
-    dashboard.button("m", "    Mason"    , ":Mason<CR>"),
-    dashboard.button("u", "    Update"   , ":TSUpdate<CR>:PackerSync<CR>"),
-    dashboard.button("q", "    Quit"     , ":qa<CR>"),
+    dashboard.button("e", "    New file" , ":ene<CR>"),
+    dashboard.button("f", "    Find file", ":Telescope find_files<CR>"),
+    dashboard.button("t", " פּ   File Tree", ":ene<CR>:NvimTreeOpen<CR>"),
+    dashboard.button("r", "    Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button("p", "    Projects" , ":Telescope projects<CR>"),
+    dashboard.button("s", "    Settings" , ":e $MYVIMRC<CR>"),
+    dashboard.button("k", "    Keybinds" , ":Keybinds<CR>"),
+    dashboard.button("m", "    Mason"    , ":Mason<CR>"),
+    dashboard.button("u", "    Update"   , ":TSUpdate<CR>:PackerSync<CR>"),
+    dashboard.button("q", "    Quit"     , ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
