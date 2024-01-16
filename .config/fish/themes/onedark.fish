b404234fb79646e8d8e1bc9694e5a54531e3bc19
mihneabuz <mihneabuzatu88@gmail.com>
2024-01-17 01:28:24 +0200

    cleanup fish themes

--- .config/fish/themes/onedark.fish
+++ .config/fish/themes/onedark.fish
@@ -1,26 +1,26 @@
 # Upstream: https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_night.fish
 # Syntax Highlighting Colors
-set -Ux fish_color_normal normal
-set -Ux fish_color_command --bold blue
-set -Ux fish_color_keyword magenta
-set -Ux fish_color_quote green
-set -Ux fish_color_redirection --bold yellow
-set -Ux fish_color_end --bold yellow
-set -Ux fish_color_error red
-set -Ux fish_color_param --bold brwhite
-set -Ux fish_color_valid_path --bold brwhite
-set -Ux fish_color_comment 5c6370
-set -Ux fish_color_selection --background=43567f
-set -Ux fish_color_search_match --background=43567f
-set -Ux fish_color_operator --bold red
-set -Ux fish_color_escape yellow
-set -Ux fish_color_autosuggestion 5c6370
+set -x fish_color_normal normal
+set -x fish_color_command --bold blue
+set -x fish_color_keyword magenta
+set -x fish_color_quote green
+set -x fish_color_redirection --bold yellow
+set -x fish_color_end --bold yellow
+set -x fish_color_error red
+set -x fish_color_param --bold brwhite
+set -x fish_color_valid_path --bold brwhite
+set -x fish_color_comment 5c6370
+set -x fish_color_selection --background=43567f
+set -x fish_color_search_match --background=43567f
+set -x fish_color_operator --bold red
+set -x fish_color_escape yellow
+set -x fish_color_autosuggestion 5c6370
 
 # Completion Pager Colors
-set -Ux fish_pager_color_progress 565f89
-set -Ux fish_pager_color_prefix --bold blue
-set -Ux fish_pager_color_completion brwhite
-set -Ux fish_pager_color_description 5c6370
-set -Ux fish_pager_color_selected_background --background=43567f
+set -x fish_pager_color_progress 565f89
+set -x fish_pager_color_prefix --bold blue
+set -x fish_pager_color_completion brwhite
+set -x fish_pager_color_description 5c6370
+set -x fish_pager_color_selected_background --background=43567f
 
 export COLORSCHEME=onedark
--- .config/fish/themes/tokyonight.fish
+++ .config/fish/themes/tokyonight.fish
@@ -1,26 +1,26 @@
 # Upstream: https://github.com/folke/tokyonight.nvim/blob/main/extras/fish/tokyonight_night.fish
 # Syntax Highlighting Colors
-set -Ux fish_color_normal normal
-set -Ux fish_color_command --bold blue
-set -Ux fish_color_keyword magenta
-set -Ux fish_color_quote green
-set -Ux fish_color_redirection --bold yellow
-set -Ux fish_color_end --bold yellow
-set -Ux fish_color_error red
-set -Ux fish_color_param --bold brwhite
-set -Ux fish_color_valid_path cyan
-set -Ux fish_color_comment 565f89
-set -Ux fish_color_selection --background=33467c
-set -Ux fish_color_search_match --background=33467c
-set -Ux fish_color_operator --bold red
-set -Ux fish_color_escape yellow
-set -Ux fish_color_autosuggestion 565f89
+set -x fish_color_normal normal
+set -x fish_color_command --bold blue
+set -x fish_color_keyword magenta
+set -x fish_color_quote green
+set -x fish_color_redirection --bold yellow
+set -x fish_color_end --bold yellow
+set -x fish_color_error red
+set -x fish_color_param --bold brwhite
+set -x fish_color_valid_path cyan
+set -x fish_color_comment 565f89
+set -x fish_color_selection --background=33467c
+set -x fish_color_search_match --background=33467c
+set -x fish_color_operator --bold red
+set -x fish_color_escape yellow
+set -x fish_color_autosuggestion 565f89
 
 # Completion Pager Colors
-set -Ux fish_pager_color_progress 565f89
-set -Ux fish_pager_color_prefix 7dcfff
-set -Ux fish_pager_color_completion c0caf5
-set -Ux fish_pager_color_description 565f89
-set -Ux fish_pager_color_selected_background --background=33467c
+set -x fish_pager_color_progress 565f89
+set -x fish_pager_color_prefix 7dcfff
+set -x fish_pager_color_completion c0caf5
+set -x fish_pager_color_description 565f89
+set -x fish_pager_color_selected_background --background=33467c
 
 export COLORSCHEME=tokyonight
