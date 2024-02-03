21c2f663b9f0cebed82763121a3e2bcca9cf658b
mihneabuz <mihneabuzatu88@gmail.com>
2024-02-04 01:27:25 +0200

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,18 @@
 set TTY (tty)
 if [ "$(tty)" = "/dev/tty1" ]
-    set -gx _JAVA_AWT_WM_NONREPARENTING 1
-    set -gx XCURSOR_SIZE 24
+  set -gx _JAVA_AWT_WM_NONREPARENTING 1
+  set -gx XCURSOR_SIZE 24
 
-    set -gx LIBVA_DRIVER_NAME nvidia
-    set -gx XDG_SESSION_TYPE wayland
-    set -gx GBM_BACKEND nvidia-drm
-    set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
-    set -gx WLR_NO_HARDWARE_CURSORS 1
+  set -gx LIBVA_DRIVER_NAME nvidia
+  set -gx XDG_SESSION_TYPE wayland
+  set -gx GBM_BACKEND nvidia-drm
+  set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
+  set -gx WLR_NO_HARDWARE_CURSORS 1
 
-    set -gx GTK_THEME Adwaita:dark
-    set -gx QT_STYLE_OVERRIDE adwaita
+  set -gx GTK_THEME Adwaita:dark
+  set -gx QT_STYLE_OVERRIDE adwaita
 
-    exec Hyprland
+  exec Hyprland
 end
 
 if status is-interactive
@@ -34,9 +34,12 @@ alias vim="nvim"
 alias lg="lazygit"
 alias lf="lfcd"
 alias venv="python -m venv"
-alias ssh="kitty +kitten ssh"
 alias dockerstart="sudo systemctl start docker"
 alias exa="exa -s type"
+
+if set -q KITTY_WINDOW_ID
+  alias ssh="kitty +kitten ssh"
+end
 
 # ls additions
 source ~/.config/fish/ls.fish
@@ -60,6 +63,7 @@ fish_add_path /usr/local/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.ghcup/bin
 fish_add_path ~/.cargo/bin
+fish_add_path ~/go/bin/
 fish_add_path /usr/lib/rustup/bin
 fish_add_path /usr/lib/ruby/gems/3.0.0
 fish_add_path /opt/homebrew/bin
