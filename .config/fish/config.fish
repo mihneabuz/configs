f80b8314be009190ece21103f794623643581185
mihneabuz <mihneabuzatu88@gmail.com>
2024-03-04 23:41:43 +0200

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,47 +16,41 @@ if [ "$(tty)" = "/dev/tty1" ]
 end
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
+
+  # alias
+  alias vi="nvim"
+  alias vim="nvim"
+  alias lg="lazygit"
+  alias lf="lfcd"
+  alias venv="python -m venv"
+  alias dockerstart="sudo systemctl start docker"
+  alias exa="exa -s type"
+
+  if set -q KITTY_WINDOW_ID
+    alias ssh="kitty +kitten ssh"
+  end
+
+  # ls additions
+  source ~/.config/fish/ls.fish
+  set -gx LSCOLORS ExFxdxdxCxagababagacax
+
+  # lf integration
+  source ~/.config/fish/lfcd.fish
+
+  # theme
+  source ~/.config/fish/themes/current.fish
+
+  # prompt
+  set -gx STARSHIP_LOG error
+  starship init fish | source
 end
 
-# fish default
-set SHELL "fish"
-
-# editor
-set -x EDITOR "nvim"
-
-# mode
-fish_vi_key_bindings
-
-# alias
-alias vi="nvim"
-alias vim="nvim"
-alias lg="lazygit"
-alias lf="lfcd"
-alias venv="python -m venv"
-alias dockerstart="sudo systemctl start docker"
-alias exa="exa -s type"
-
-if set -q KITTY_WINDOW_ID
-  alias ssh="kitty +kitten ssh"
-end
-
-# ls additions
-source ~/.config/fish/ls.fish
-set -gx LSCOLORS ExFxdxdxCxagababagacax
+set -gx SHELL "fish"
+set -gx EDITOR "nvim"
 
 # greeting
 set fish_greeting ""
-
-# lf integration
-source ~/.config/fish/lfcd.fish
-
-# theme
-source ~/.config/fish/themes/current.fish
-
-# prompt
-export STARSHIP_LOG=error
-starship init fish | source
 
 # paths
 fish_add_path /usr/local/bin
@@ -68,9 +62,6 @@ fish_add_path /usr/lib/rustup/bin
 fish_add_path /usr/lib/ruby/gems/3.0.0
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
-
-# rust cache
-export RUSTC_WRAPPER=sccache
 
 # pnpm
 set -gx PNPM_HOME "/home/mihnea/.local/share/pnpm"
