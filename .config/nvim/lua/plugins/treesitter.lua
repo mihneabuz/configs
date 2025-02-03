44764e1c13594b57b5c942e21e172b0ef5ea7a8e
mihneabuz <mihneabuzatu88@gmail.com>
2025-02-03 10:18:58 +0200

    remove nvim-ufo

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -77,47 +77,4 @@ return {
     },
     config = true
   },
-
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
-    keys = {
-      { "za", desc = "Toggle fold" },
-      { "zM", function() require("ufo").openAllFolds() end,  desc = "Open all folds" },
-      { "zN", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
-    },
-    main = "ufo",
-    opts = {
-      open_fold_hl_timeout = 100,
-      provider_selector = function(_, _, _)
-        return { "treesitter" }
-      end,
-      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-        local newVirtText = {}
-        local suffix = ("  󱞦 %d "):format(endLnum - lnum)
-        local sufWidth = vim.fn.strdisplaywidth(suffix)
-        local targetWidth = width - sufWidth
-        local curWidth = 0
-        for _, chunk in ipairs(virtText) do
-          local chunkText = chunk[1]
-          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-          else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if curWidth + chunkWidth < targetWidth then
-              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-          end
-          curWidth = curWidth + chunkWidth
-        end
-        table.insert(newVirtText, { suffix, "MoreMsg" })
-        return newVirtText
-      end
-    }
-  },
 }
